fa2ed5af7a5bbf62394b5d8dffada1f96df253f5
Alexander Kireev <sashkir7@mail.ru>
2022-12-16 18:16:47 +0600

    Add user properties

---
+++ src/test/java/config/UserProperties.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class UserProperties {
+
+    private static final UserConfig CONFIG = ConfigFactory.create(UserConfig.class, System.getProperties());
+
+    public static final String USERNAME = CONFIG.username(),
+            PASSWORD = CONFIG.password(),
+            TOKEN = CONFIG.token();
+
+}
